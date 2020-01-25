f07c9d2424f0ee229b7c374f0622a20fa6b9dee2
FRC1555 <59070708+FRC1555@users.noreply.github.com>
2020-01-25 13:12:51 -0500

    Updated to work with the new RobotMap.java

--- robot/subsystems/DriveTrain.java
+++ robot/subsystems/DriveTrain.java
@@ -15,43 +15,31 @@ public class DriveTrain extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
-	//Declaring motors
-	//Due to a lack of signal wire splitters, we currently have two ports for the right drive motors
-	public static Victor L;
-    public static Victor R;
-    public static Victor R2;
+    //Declaring motors
+    public static Victor L = robot.map.leftMotor;
+    public static Victor R = robot.map.Rightmotor;
     
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
     
-    //Called during init to create the drive train
-    public static void buildDrive() {
-    	L = new Victor(RobotMap.Lmotor);
-    	R = new Victor(RobotMap.Rmotor);
-    	R2 =  new Victor(RobotMap.Rmotor2);
-    }
-    
     //Stops the drive train
     public static void stop() {
     	L.stopMotor();
         R.stopMotor();
-    	R2.stopMotor();
     }
     
     //Drives the robot with two separate powers for the wheels
     public static void driveTank(double Lspeed, double Rspeed) {
     	L.set(-Lspeed);
     	R.set(Rspeed);
-    	R2.set(Rspeed);
     }
     
     //Drives straight
     public static void driveStraight(double speed) {
     	L.set(-speed);
     	R.set(speed);
-    	R2.set(speed);
     }
     
     //Function pending
