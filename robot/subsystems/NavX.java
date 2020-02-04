f5e23620a1a3fb437881e7f28bec26902b16ba32
FRC1555 <59070708+FRC1555@users.noreply.github.com>
2020-02-03 20:27:47 -0500

    Adding NavX subsystem

---
+++ robot/subsystems/NavX.java
@@ -0,0 +1,33 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.Robot;
+//Find out why we can't use this import
+//import edu.wpi.first.wpilibj2.PIDController;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * An example subsystem.  You can replace me with your own Subsystem.
+ */
+public class NavX extends Subsystem {
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+    AHRS ahrs = Robot.map.ahrs;
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public double heading() {
+        return ahrs.getAngle();
+    }
+}
