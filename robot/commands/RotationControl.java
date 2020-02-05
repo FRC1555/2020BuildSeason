7854ed3eec71076346489aa2acba559e797fd6b2
JosephPatus <59898578+JosephPatus@users.noreply.github.com>
2020-02-04 20:29:54 -0500

    Add files via upload
    Completed version of the Rotation Control Command.

--- robot/commands/RotationControl.java
+++ robot/commands/RotationControl.java
@@ -7,18 +7,30 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 import frc.robot.subsystems.ColorSensor;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * An example command.  You can replace me with your own command.
  */
 public class RotationControl extends Command {
     //
-    String Color; 
-    //
-    String[] colSeq = ("Green", "Blue", "Yellow", "Red");
+	String startColor; 
+	//
+	String Color2;
+
+	Victor spinner = Robot.map.spinner;
+	
+	//String[] col = ("Green", "Blue", "Yellow", "Red");
+	
+	//
+	boolean Flag;
+	//
+	int Ticks;
     
     public RotationControl() {
 		// Use requires() here to declare subsystem dependencies
@@ -28,30 +40,53 @@ public class RotationControl extends Command {
 	// Called just before this Command runs the first time
 	@Override
 	protected void initialize() {
+		startColor = "Unknown";
+		Flag = false;
+		Ticks = 0;
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	@Override
 	protected void execute() {
-        // Spin for 4 rotations
+		// Spin for 4 rotations
         // Store the current color
-        Color = Robot.eyeball.RobotColorDetector();
-        // Check for a color change
-        // Check to ensure that new color is the one that is anticipated
-        // Compare the new color to the stored previous color, replace old color with new one
-        // Add a 'tick' to the running total once the change has taken place
-        // Stop after 32 ticks have been accumulated
+		if (startColor == "Unknown") {
+			startColor = Robot.eyeball.RobotColorDetector();
+			return;
+		}
+		spinner.set(1);
+
+		// Check for a color change
+		
+		Color2 = Robot.eyeball.RobotColorDetector();
+		
+		if (Color2 == startColor) {
+			if (!Flag) {
+				Ticks += 1;
+			}
+			Flag = true;
+		}
+		else {
+			Flag = false;
+		}
+		// Stop after 8 ticks have been accumulated
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	@Override
 	protected boolean isFinished() {
-		return false;
+		if (Ticks == 8) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 	// Called once after isFinished returns true
 	@Override
 	protected void end() {
+		spinner.set(0);
 	}
 
 	// Called when another command which requires one or more of the same
