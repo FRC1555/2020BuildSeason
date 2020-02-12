c13a23724053bce44ae8ad1c2693b29bb27e744e
FRC1555 <59070708+FRC1555@users.noreply.github.com>
2020-02-11 20:41:59 -0500

    Revised encoders
    Moved declaration of encoders to robotMap, and added new ones.

--- robot/RobotMap.java
+++ robot/RobotMap.java
@@ -10,11 +10,15 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.CAN;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import com.revrobotics.ColorSensorV3;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.ColorMatchResult;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
 import com.revrobotics.ColorMatch;
 
 
@@ -39,28 +43,49 @@ public class RobotMap {
 	//Declaring the elements of the drive train
 	public static Victor leftMotor;
 	public static Victor rightMotor;
-	public static int Lmotor = 0;
-	public static int Rmotor = 1;
+	int Lmotor = 0;
+	int Rmotor = 1;
 	
-	//
+	//Shooter motors - CAN
+	//Intake
+	//Intake lift
+	//Climb lift
+	//Balancer
+	//CPI lift
+	//CPI spinner
+
+	public CANSparkMax shooter1;
+	public CANSparkMax shooter2;
+	int shooter1Index = 0;
+	int shooter2Index = 1;
+
 	public static Talon intakeMotor;
 	public static Talon liftMotor;
-	public static int intake = 2;
-	public static int intakeLift = 3;
+	int intake = 2;
+	int intakeLift = 3;
 	//The hatch panel grabbers are currently run off two ports, although we should be able to change it to one in the future
-	public static int hatchSlapper1 = 4;
-	public static int hatchSlapper2 = 5;
+	int hatchSlapper1 = 4;
+	int hatchSlapper2 = 5;
 	
+	//Transfer the build over to here
 	public static int solenoidP1 = 0;
 	public static int solenoidP2 = 1;
 	
-	public static int Lencoder = 0;
-	public static int Rencoder = 1;
+	public static Encoder encDriveL;
+	public static Encoder encDriveR;
+	public static Encoder encLiftCPI;
+	public static Encoder encLiftClimber;
+	int encDriveL1 = 0;
+	int encDriveL2 = 1;
+	int encDriveR1 = 2;
+	int encDriveR2 = 3;
+	int encLiftCPI1 = 4;
+	int encLiftCPI2 = 5;
+	int encLiftClimber1 = 6;
+	int encLiftClimber2 = 7;
 
-	public static int sensorA = 2;
-	public static int sensorB = 3;
-	public static int sensorC = 4;
-	public static int sensorD = 5;
+	public static CANEncoder encShooterA;
+	public static CANEncoder encShooterB;
 	
 	public static String limeLightKey = "limelight";
 	
@@ -74,14 +99,25 @@ public class RobotMap {
 
 	//Initalizes all the hardware
 	public void mapAll() {
+		//Motor controllers
 		leftMotor = new Victor(Lmotor);
 		rightMotor = new Victor(Rmotor);
 		intakeMotor = new Talon(intake);
 		liftMotor = new Talon(intakeLift);
 		hatchMotor = new Talon(hatchSlapper1);
 		slapMotor = new Talon(hatchSlapper2);
+		shooter1 = new CANSparkMax(shooter1Index, MotorType.kBrushless);
+		shooter2 = new CANSparkMax(shooter2Index, MotorType.kBrushless);
+		//Misc sensors
 		colourSensor = new ColorSensorV3(i2cPort);
 		colourMatch = new ColorMatch();
+		//Encoders
+		encDriveL = new Encoder(encDriveL1, encDriveL2);
+		encDriveR = new Encoder(encDriveR1, encDriveR2);
+		encLiftCPI = new Encoder(encLiftCPI1, encLiftCPI2);
+		encLiftClimber = new Encoder(encLiftClimber1, encLiftClimber2);
+		encShooterA = shooter1.getEncoder();
+		encShooterB = shooter2.getEncoder();
 	}
 
 	public ColorMatchResult matchClosestColor(Object detectedColor) {
