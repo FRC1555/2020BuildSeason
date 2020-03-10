b4e614f4109858fc44f22f4b407bad421f60c589
YaBoiMiercoles <59630896+YaBoiMiercoles@users.noreply.github.com>
2020-03-09 20:52:44 -0400

    Add files via upload

--- robot/commands/NavXSeeker.java
+++ robot/commands/NavXSeeker.java
@@ -7,8 +7,11 @@
 
 package frc.robot.commands;
 
+import org.opencv.core.Mat;
+
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
+
 
 
 /**
@@ -16,45 +19,78 @@ import frc.robot.Robot;
  */
 public class NavXSeeker extends Command {
 
-	public double lastRecorded0;
+    //This value will hold the value for the angle that was last recorded before the limelight was not within distance of the target
+    public double lastRecorded0;
 
-	double Angle;
-	//speed for the Driving motors before adjustments
-	public final double kLDriveSpeed = 0.2;
+    //This value will hold the current recorded angle from the navx
+    public double current0;
+
+    //default speed for the motors
+    public final double kdrivespeed = 0.2;
+    
+    //Turning speed for the robot when aligning itself for the last recorded 0 
+	double turnSpeed = 0.25;
+
+    //This value will have the power adjustment for the motors
+    double speedAdjust;
+    double steeringAdjust;
 
 	//speeds to give for the drive motors(after adjustments)
 	double lspeed;
-	double rspeed;
-	
-	//steering adjustment speed for the robot
-	double turnSpeed = 0.25;
+    double rspeed;
+    
+    //max value of steering adjust
+    public final double steeringcap = 0.3;
+
+    //sets the difference in angle to 0 if the absolute value of the angle is less than 3 degrees away from the target angle
+    public final double Angletolerancethreshold = 3;
+    
+    public static SeekVisionTarget kSeekVisionTarget
+    = new SeekVisionTarget();
+
+    boolean recordtarget0;
 
 	public NavXSeeker() {
-
-		//requires(Robot.kExampleSubsystem);
+    //requires(Robot.kExampleSubsystem);
 		requires(Robot.kNavX);
-		
 	}
 
 	// Called just before this Command runs the first time
 	@Override
 	protected void initialize() {
+       steeringAdjust = 0;
+       current0 = Robot.kNavX.getIMUPitch();
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	@Override
 	protected void execute() {
 		//This sets the last recorded Pitch(left to right turns) that the robot was at before it lost track of the target when it was using the lime light 
-		if(Robot.kSeekVisionTarget.isFinished() == true){
-			lastRecorded0 = Robot.kNavX.getIMUPitch();
-			Robot.Drive.driveTank(lspeed, rspeed);
-		}
+		if(Robot.kSeekVisionTarget.reachedTargetDistance == true){
+            lastRecorded0 = Robot.kNavX.getIMUPitch();
+            recordtarget0 = false;    
+        }    
+        //adjusting the robot to be at the correct angle when moving towards the target
+        steeringAdjust = Math.abs(current0) - Math.abs(lastRecorded0);
+        if(steeringAdjust > steeringcap){
+            steeringAdjust = steeringcap;
+        }
+        else if(steeringAdjust < -steeringcap){
+            steeringAdjust = -steeringcap;
+        }
+        lspeed = steeringAdjust - kdrivespeed;
+        rspeed = steeringAdjust + kdrivespeed;
+        //speed of the robot
+        Robot.Drive.driveTank(lspeed, rspeed);
+        if(steeringAdjust > 10){
+            Robot.Drive.driveTank(turnSpeed, -turnSpeed);
+        }
+        else if(steeringAdjust <-10){
+            Robot.Drive.driveTank(-turnSpeed, turnSpeed);
 
-		//This portion of the code is used for keeping the robot on track towards the target
-
-
-		
-	}
+        }
+        
+        }
 
 	// Make this return true when this Command no longer needs to run execute()
 	@Override
